80d966d4c62ec18826dd17e90205a4d31f77abe8
KaliLinuxIII <128271072+KaliLinuxIII@users.noreply.github.com>
2024-06-26 17:19:03 -0400

    #

---
+++ main.js
@@ -0,0 +1,24 @@
+/**@type {HTMLInputElement} */
+const nameInputElement = document.getElementById('name-input-element');
+/**@type {HTMLParagraphElement} */
+const nameDisplay = document.getElementById('name-display');
+/**@type {HTMLButtonElement} */
+const addBtn = document.getElementById('add-btn');
+/**@type {HTMLButtonElement} */
+const randomNameChooserBtn = document.getElementById('random-name-chooser-btn');
+
+const names = [];
+
+function addNames() {
+  const nameInput = nameInputElement.value;
+  names.push(nameInput);
+};
+
+function chooseName() {
+  const randomIndex = Math.floor(Math.random() * names.length);
+  const randomName = names[randomIndex];
+  nameDisplay.textContent = randomName;
+};
+
+addBtn.addEventListener('click', addNames);
+randomNameChooserBtn.addEventListener('click', chooseName);
